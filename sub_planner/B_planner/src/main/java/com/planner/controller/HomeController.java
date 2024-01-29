8100d291afd10915c28d6de561ae85ec94c9d079
CJeaHo <ekrxj35@gmail.com>
2024-01-29 10:19:26 +0900

    login user와 합치기 전(insert, 미니달력, 메인달력 연동까지)

---
+++ sub_planner/B_planner/src/main/java/com/planner/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.planner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes({"loginMember"}) // 로그인은 유지되어야하는데 model은 일시적이여서 계속 유지가 안되므로 세션에 넣어 처리
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String root() throws Exception {
+		return "home";
+	}
+	
+	@RequestMapping("home")
+	public String home() throws Exception {
+		return "home";
+	}
+	
+	@GetMapping("select")
+	public String calendarForm() {
+		return "calendar/mCalendar";
+	}
+	
+}
---
+++ sub_planner/B_planner/src/main/java/com/planner/controller/PlannerController.java
@@ -0,0 +1,62 @@
+package com.planner.controller;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.planner.domain.Plan;
+import com.planner.service.PlannerService;
+
+import jakarta.servlet.http.HttpSession;
+
+
+@Controller
+public class PlannerController {
+	
+	@Autowired 
+	public PlannerService plannerService;
+	
+	// 일정 추가
+	@PostMapping("insert")
+	@ResponseBody 
+	public String insert(@RequestParam("title") String title,
+						 @RequestParam("startDate") LocalDate startDate,
+						 @RequestParam("startTime") String startTime,
+						 @RequestParam("endDate") LocalDate endDate,
+						 @RequestParam("endTime") String endTime,
+						 @RequestParam("content") String content,
+						 @RequestParam("place") String place,
+						 @RequestParam("userEmail") String userEmail,
+						 Plan plan) { 
+		
+		plan.setTitle(title);
+		plan.setStartDate(startDate);
+		plan.setStartTime(startTime);
+		plan.setEndDate(endDate);
+		plan.setEndTime(endTime);
+		plan.setContent(content);
+		plan.setPlace(place);
+		plan.setUserEmail(userEmail);
+		
+		plannerService.insert(plan);
+		return "success"; 
+	}
+	
+	/*
+	 * // 일정 조회
+	 * 
+	 * @GetMapping("select") public String select(@RequestParam(value="userEmail")
+	 * String userEmail, HttpSession session , Model model) {
+	 * 
+	 * if(session.getAttribute("loginUser") != null) { }
+	 * model.addAttribute("userPlan", plannerService.select(userEmail)); return
+	 * "calendar/mCalendar"; }
+	 */
+	
+}
