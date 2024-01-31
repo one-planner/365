d9c82229bc16e2b2e1af0aff1fbd80c40401540a
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-31 10:32:02 +0900

    email verifcation update

--- OnePlanner2/src/main/java/com/study/etc/UserController.java
+++ OnePlanner2/src/main/java/com/study/etc/UserController.java
@@ -1,47 +1,37 @@
 /*
- * 이메일 인증을 위한 기능을 제공하는 컨트롤러 (by min)
+ * 사용자 관련 기능을 처리하는 RESTful API를 제공하는 UserController 클래스 (by min)
  */
-package com.study.controller;
+package com.study.etc;
 
 import java.security.SecureRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.study.service.EmailService;
 
-@Controller
-public class EmailVerificationController {
+@RestController
+public class UserController {
 	
     @Autowired
     private EmailService emailService;
-	
-    @GetMapping("/email-verification-form")
-    public String showEmailVerificationForm() {
-        return "email-verification-form";
-    }
 
-    @PostMapping("/verify-email")   //email-verification-form.html
-    public String verifyEmail(@RequestParam("email") String email) {
+    @PostMapping("/send-verification-email")
+    public String sendVerificationEmail(@RequestParam String email) {
     	
-    	// 사용자 이메일로 랜덤인증코드 생성
+        // 사용자 이메일로 랜덤인증코드 생성
         String verificationCode = generateRandomCode();
         
         // 사용자 이메일로 인증코드 전송
         emailService.sendVerificationEmail(email, verificationCode);
-        
-        // 로그메시지 추가
-        System.out.println("해당 이메일로 전송완료: " + email);
-        System.out.println("인증코드: " + verificationCode);
-        
-        return "verification-email-sent";  //email-verification-form.html
+
+        return "이메일을 성공적으로 전송했습니다!";
     }
 
-	private String generateRandomCode() {
-		
+    private String generateRandomCode() {
+    	
         // 랜덤코드 생성
 		String digits = "0123456789";
 		int codeLength = 4; //코드길이
@@ -54,7 +44,6 @@ public class EmailVerificationController {
 			randomCode.append(digits.charAt(randomIndex));			
 		}
 		return randomCode.toString();
-	}
-	
+    }
 
 }
